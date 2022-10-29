6490d64cf0be35bb83f9c8b47c6900a2acb7d366
2612priyankababu <116347966+2612priyankababu@users.noreply.github.com>
2022-10-29 18:48:50 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,17 @@
+package Irc;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		 
+		 int val3=10;
+		 int val4=20;
+		 
+		 // incrementing both pre and post fix
+		System.out.println("incrementing both pre and post fix");
+		System.out.println("pre-incrementing val1 :  "+ ++val3);
+		System.out.println("pre-incrementing val2 :  "+ ++val4);
+		System.out.println("post-incrementing val1 :  "+ val3++);
+		System.out.println("post-incrementing val2 :  "+ val4++);
+	}
+	}
